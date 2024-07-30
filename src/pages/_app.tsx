ccf7e2ffbdf7befbf90b29d5a593f4df9a7eb024
szilardjanko <szilard.janko@gmail.com>
2024-07-30 19:09:57 -0400

    text white

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
       <SideBarProvider>
-        <div className="flex min-h-screen flex-col bg-gradient-to-bl from-slate-800 to-black">
+        <div className="flex min-h-screen flex-col bg-gradient-to-bl from-slate-800 to-black text-white">
           <NavBar />
           <div className="flex-grow">
             <Component {...pageProps} />
