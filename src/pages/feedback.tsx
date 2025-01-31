282a5fa2f83858348176b2a0e8b57c0f53c124ef
szilardjanko <szilard.janko@gmail.com>
2025-01-31 12:53:39 -0500

    feedback page

---
+++ src/pages/feedback.tsx
@@ -0,0 +1,15 @@
+export default function Feedback() {
+  return (
+    <div className="mx-auto flex flex-col items-center px-4 py-8">
+      <div className="">
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLScW0s5xlgJTcKUiz7GstVNSFAiVgoGQyC4UWbe9g2ngXnv2cw/viewform?embedded=true"
+          width="640"
+          height="859"
+        >
+          Loading…
+        </iframe>
+      </div>
+    </div>
+  );
+}
